11c844588af8861d00c83d1c12048ffad8485521
Miguel Londoño <magnolinneo1971@gmail.com>
2026-04-21 17:25:33 -0500

    Se piden los datos del cliente para guardar en la base de datos

--- src/main/java/com/proyecto/Main.java
+++ src/main/java/com/proyecto/Main.java
@@ -5,25 +5,33 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 
 import com.proyecto.config.ConexionMySQLDatabase;
+import com.proyecto.controlador.controladorUsuario;
+import com.proyecto.vista.vistaUsuario;
+
+
 
 public class Main {
     public static void main(String[] args) {
-        try (Connection conn = ConexionMySQLDatabase.getConnection()) {
-            if (conn != null) {
-                System.out.println("✅ Conexión establecida correctamente.");
+        // try (Connection conn = ConexionMySQLDatabase.getConnection()) {
+        // if (conn != null) {
+        // System.out.println("✅ Conexión establecida correctamente.");
 
-                // Ejemplo: ejecutar una consulta
-                Statement stmt = conn.createStatement();
-                ResultSet rs = stmt.executeQuery("SELECT NOW() AS fecha_actual");
+        // // Ejemplo: ejecutar una consulta
+        // Statement stmt = conn.createStatement();
+        // ResultSet rs = stmt.executeQuery("SELECT NOW() AS fecha_actual");
 
-                while (rs.next()) {
-                    System.out.println("La base de datos respondió: " + rs.getString("fecha_actual"));
-                }
-            } else {
-                System.out.println("❌ No se pudo establecer la conexión.");
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        // while (rs.next()) {
+        // System.out.println("La base de datos respondió: " +
+        // rs.getString("fecha_actual"));
+        // }
+        // } else {
+        // System.out.println("❌ No se pudo establecer la conexión.");
+        // }
+        // } catch (Exception e) {
+        // e.printStackTrace();
+        // }
+
+        vistaUsuario vista = new vistaUsuario();
+        vista.registrarUsuario();
     }
 }
