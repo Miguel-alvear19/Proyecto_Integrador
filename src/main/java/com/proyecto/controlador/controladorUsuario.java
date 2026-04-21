ecb2e9e7754e0a46000d5c2b532fdc949724eafe
Miguel Londoño <magnolinneo1971@gmail.com>
2026-04-21 17:28:06 -0500

    Se crea el controladorUsuario para registrar el usuario

---
+++ src/main/java/com/proyecto/controlador/controladorUsuario.java
@@ -0,0 +1,18 @@
+package com.proyecto.controlador;
+
+import com.proyecto.DAO.usuarioDao;
+import com.proyecto.modelo.Registrarse;
+
+public class controladorUsuario {
+    private usuarioDao usuarioDao;
+
+    public controladorUsuario() {
+        usuarioDao = new usuarioDao();
+    }
+
+    public void registrarUsuario(String nombre, String apellido, String correo, int numeroCedula,
+                                 String telefono, String usuario, String contraseña) {
+        Registrarse nuevoUsuario = new Registrarse(nombre, apellido, correo, numeroCedula, telefono, usuario, contraseña);
+        usuarioDao.insertarUsuario(nuevoUsuario);
+    }
+}
