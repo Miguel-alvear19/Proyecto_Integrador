f294b4225d665a0c47addae15cf23a914f051118
Miguel Londoño <magnolinneo1971@gmail.com>
2026-04-21 17:29:42 -0500

    Se corrigen errores de tipo de dato y ortografia

--- src/main/java/com/proyecto/modelo/Registrarse.java
+++ src/main/java/com/proyecto/modelo/Registrarse.java
@@ -5,9 +5,77 @@ public class Registrarse {
     private String nombre;
     private String apellido;
     private String correo;
-    private int NumeroCedula;
-    private int telefono;
+    private int numeroCedula;
+    private String telefono;
     protected String Usuario;
     protected String Contraseña;
+
+    public Registrarse(String nombre, String apellido, String correo, int numeroCedula, String telefono, String usuario, String contraseña) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.correo = correo;
+        this.numeroCedula = numeroCedula;
+        this.telefono = telefono;
+        this.Usuario = usuario;
+        this.Contraseña = contraseña;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public int getNumeroCedula() {
+        return numeroCedula;
+    }
+
+    public void setNumeroCedula(int numeroCedula) {
+        this.numeroCedula = numeroCedula;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getUsuario() {
+        return Usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        Usuario = usuario;
+    }
+
+    public String getContraseña() {
+        return Contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        Contraseña = contraseña;
+    }
+
+    
 
 }
