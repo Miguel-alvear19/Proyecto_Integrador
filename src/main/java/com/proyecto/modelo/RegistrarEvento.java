423f690985b269a691f1d1b558290b931798d60d
Miguel Londoño <magnolinneo1971@gmail.com>
2026-04-22 15:47:12 -0500

    Modelo RegistarEvento donde se crean metodo get y set con su respectivo constructor

--- src/main/java/com/proyecto/modelo/RegistrarEvento.java
+++ src/main/java/com/proyecto/modelo/RegistrarEvento.java
@@ -6,6 +6,57 @@ public class RegistrarEvento {
     private int capacidadMaxima;
     private String lugarEvento;
     private String descripcion;
-    private int telefonoContacto;
+    private String telefonoContacto;
+
+    public RegistrarEvento(String nombreEvento, int capacidadMaxima, String lugarEvento, String descripcion,
+            String telefonoContacto2) {
+        this.nombreEvento = nombreEvento;
+        this.capacidadMaxima = capacidadMaxima;
+        this.lugarEvento = lugarEvento;
+        this.descripcion = descripcion;
+        this.telefonoContacto = telefonoContacto2;
+    }
+
+    public String getNombreEvento() {
+        return nombreEvento;
+    }
+
+    public void setNombreEvento(String nombreEvento) {
+        this.nombreEvento = nombreEvento;
+    }
+
+    public int getCapacidadMaxima() {
+        return capacidadMaxima;
+    }
+
+    public void setCapacidadMaxima(int capacidadMaxima) {
+        this.capacidadMaxima = capacidadMaxima;
+    }
+
+    public String getLugarEvento() {
+        return lugarEvento;
+    }
+
+    public void setLugarEvento(String lugarEvento) {
+        this.lugarEvento = lugarEvento;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getTelefonoContacto() {
+        return telefonoContacto;
+    }
+
+    public void setTelefonoContacto(String telefonoContacto) {
+        this.telefonoContacto = telefonoContacto;
+    }
+
+    
 
 }
