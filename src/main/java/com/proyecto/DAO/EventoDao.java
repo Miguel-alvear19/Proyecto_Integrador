dcf11bc09457c3b45fc7111aa537a85914f45707
Miguel Londoño <magnolinneo1971@gmail.com>
2026-05-19 10:42:34 -0500

    Se le añade a la sentencia select el campo idEvento

--- src/main/java/com/proyecto/DAO/EventoDao.java
+++ src/main/java/com/proyecto/DAO/EventoDao.java
@@ -12,31 +12,32 @@ import com.proyecto.modelo.RegistrarEvento;
 
 public class EventoDao {
     public boolean insertarEvento(RegistrarEvento evento) {
-        try (Connection conn = ConexionMySQLDatabase.getConnection()) {
-            String sql = "INSERT INTO eventos (nombreEvento, capacidadMaxima, lugarEvento, descripcion, telefonoContacto) VALUES (?, ?, ?, ?, ?)";
-            PreparedStatement ps = conn.prepareStatement(sql);
-            ps.setString(1, evento.getNombreEvento());
-            ps.setInt(2, evento.getCapacidadMaxima());
-            ps.setString(3, evento.getLugarEvento());
-            ps.setString(4, evento.getDescripcion());
-            ps.setString(5, evento.getTelefonoContacto());
-            ps.executeUpdate();
-            return true;
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return false;
-        }
+    try (Connection conn = ConexionMySQLDatabase.getConnection()) {
+        String sql = "INSERT INTO eventos (nombreEvento, capacidadMaxima, lugarEvento, descripcion, telefonoContacto) VALUES (?, ?, ?, ?, ?)";
+        PreparedStatement ps = conn.prepareStatement(sql);
+        ps.setString(1, evento.getNombreEvento());
+        ps.setInt(2, evento.getCapacidadMaxima());
+        ps.setString(3, evento.getLugarEvento());
+        ps.setString(4, evento.getDescripcion());
+        ps.setString(5, evento.getTelefonoContacto());
+        ps.executeUpdate();
+        return true;
+    } catch (SQLException e) {
+        e.printStackTrace();
+        return false;
     }
+}
 
     public List<RegistrarEvento> listarEventos() {
         List<RegistrarEvento> lista = new ArrayList<>();
         try (Connection conn = ConexionMySQLDatabase.getConnection()) {
-            String sql = "SELECT nombreEvento, lugarEvento, capacidadMaxima, descripcion, telefonoContacto FROM eventos";
+            String sql = "SELECT idEvento, nombreEvento, lugarEvento, capacidadMaxima, descripcion, telefonoContacto FROM eventos";
             PreparedStatement ps = conn.prepareStatement(sql);
             ResultSet rs = ps.executeQuery();
 
             while (rs.next()) {
                 RegistrarEvento ev = new RegistrarEvento(
+                        rs.getInt("idEvento"),
                         rs.getString("nombreEvento"),
                         rs.getInt("capacidadMaxima"),
                         rs.getString("lugarEvento"),
